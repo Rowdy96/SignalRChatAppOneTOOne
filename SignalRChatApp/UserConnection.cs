da1e05593363030eec6f5462a88ee89bd1471153
Rahul Mondal <RahulMondal@promact.com>
2019-11-04 18:12:40 +0530

    ConnectionId problem is removed and modified the code

---
+++ SignalRChatApp/UserConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SignalRChatApp
+{
+    public class UserConnection
+    {
+        public string UserId { get; set; }
+        public string ConnectionId { get; set; }
+    }
+}
